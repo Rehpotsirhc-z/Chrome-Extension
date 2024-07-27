65a6916392af7e1964dd350ed4ef99d54e1170dc
Rehpotsirhc <86068565+Rehpotsirhc-z@users.noreply.github.com>
2024-07-26 20:15:32 -0400

    Add buttons in Blocking
    Co-authored-by: Andrew Park <Adamoris@users.noreply.github.com>

--- barrier.js
+++ barrier.js
@@ -1,17 +1,17 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const passwordSection = document.getElementById('password-section');
-    const setPasswordSection = document.getElementById('set-password-section');
+document.addEventListener("DOMContentLoaded", function () {
+    const passwordSection = document.getElementById("password-section");
+    const setPasswordSection = document.getElementById("set-password-section");
 
-    const passwordInput = document.getElementById('password-field');
+    const passwordInput = document.getElementById("password-field");
 
-    const setPasswordInput = document.getElementById('set-password-field');
-    const confirmPasswordInput = document.getElementById('confirm-password-field');
-    const setPasswordButton = document.getElementById('set-password-button');
+    const setPasswordInput = document.getElementById("set-password-field");
+    const confirmPasswordInput = document.getElementById("confirm-password-field");
+    const setPasswordButton = document.getElementById("set-password-button");
 
-    const submitPasswordButton = document.getElementById('submit-password-button');
+    const submitPasswordButton = document.getElementById("submit-password-button");
 
     // Check if there is a stored password in local
-    if (localStorage.getItem('password')) {
+    if (localStorage.getItem("password")) {
         passwordSection.style.display = "flex";
         setPasswordSection.style.display = "none";
     } else {
@@ -19,28 +19,29 @@ document.addEventListener('DOMContentLoaded', function() {
         setPasswordSection.style.display = "flex";
     }
 
-    setPasswordButton.addEventListener('click', function() {
+    setPasswordButton.addEventListener("click", function () {
         const password = setPasswordInput.value;
         const confirmPassword = confirmPasswordInput.value;
 
         if (password === confirmPassword && password !== "") {
-            localStorage.setItem('password', password);
+            localStorage.setItem("password", password);
             passwordSection.style.display = "flex";
             setPasswordSection.style.display = "none";
         }
     });
 
-    submitPasswordButton.addEventListener('click', function() {
-        const password = localStorage.getItem('password');
+    submitPasswordButton.addEventListener("click", function () {
+        const password = localStorage.getItem("password");
         const attemptedPassword = passwordInput.value;
 
         if (password === attemptedPassword) {
-            localStorage.setItem('authenticated', true);
+            localStorage.setItem("authenticated", true);
             console.log(localStorage.getItem("authenticated"));
             // wait for 1 second before redirecting
-            setTimeout(() => {
-                window.location.href = "settings.html";
-            }, 1000);
+            // setTimeout(() => {
+            //     window.location.href = "settings.html";
+            // }, 1000);
+            window.location.href = "settings.html";
         }
     });
 });
--- settings.js
+++ settings.js
@@ -1,24 +1,86 @@
 function openTab(evt, tabName) {
-    var tabContent = document.getElementsByClassName("tabcontent");
-    for (var i = 0; i < tabContent.length; i++) {
-        tabContent[i].style.display = "none";
-    }
+    Array.from(document.getElementsByClassName("tabcontent"))
+        .forEach(element => element.style.display = "none");
 
-    var tabLinks = document.getElementsByClassName("tablink");
-    for (var i = 0; i < tabLinks.length; i++) {
-        tabLinks[i].className = tabLinks[i].className.replace(" active", "");
-    }
+    Array.from(document.getElementsByClassName("tablink"))
+        .forEach(element => element.className = element.className.replace(" active", ""));
 
+    // console.log("Active tabName: ", tabName);
     document.getElementById(tabName).style.display = "block";
     evt.currentTarget.className += " active";
 }
 
 
-document.addEventListener('DOMContentLoaded', function() {  
-    // console.log(localStorage.getItem('authenticated'));
-    if (localStorage.getItem('authenticated') === null || localStorage.getItem('authenticated') === "false" || localStorage.getItem('authenticated') === false) {
+function toggleExpandButton(event, sectionId) {
+    var section = document.getElementById(sectionId);
+    if (section.classList.contains("expanded")) {
+        section.classList.remove("expanded");
+        section.style.maxHeight = "0px";
+    } else {
+        section.classList.add("expanded");
+        section.style.maxHeight = section.scrollHeight + 10 + "px";
+    }
+}
+
+
+function toggleRadioButton(event) {
+    var targetID = this.getAttribute("data-target").replace("#", "");
+    let radioButton = document.getElementById(targetID);
+    radioButton.checked = true;
+}
+
+
+document.addEventListener("DOMContentLoaded", function () {
+    const logoutButton = document.getElementById("logout-button");
+    // const ages3To5ExpandButton = document.getElementById("ages-3-to-5-expand-button");
+    // const ages6To12ExpandButton = document.getElementById("ages-6-to-12-expand-button");
+    // const ages13To18ExpandButton = document.getElementById("ages-13-to-18-expand-button");
+    const expandButtonIds = [
+        "ages-3-to-5-expand-button",
+        "ages-6-to-12-expand-button",
+        "ages-13-to-18-expand-button"
+    ];
+
+
+    // console.log(localStorage.getItem("authenticated"));
+    if (localStorage.getItem("authenticated") === null || localStorage.getItem("authenticated") === "false" || localStorage.getItem("authenticated") === false) {
         window.location.href = "barrier.html";
     }
 
-    document.querySelector('.tablink').click();
+    Array.from(document.getElementsByClassName("tablink")).forEach(tabLink => {
+        tabLink.addEventListener("click", function (event) {
+            openTab(event, this.getAttribute("href").replace("#", ""));
+        });
+    });
+
+
+    document.querySelector(".tablink").click();
+
+
+    Array.from(document.getElementsByClassName("blocking-preset"))
+        .forEach(preset => preset.addEventListener("click", toggleRadioButton));
+
+
+    expandButtonIds.forEach(buttonId => {
+        const sectionId = buttonId.replace("expand-button", "expanded");
+        document.getElementById(buttonId).addEventListener("click", event => 
+            toggleExpandButton(event, sectionId)
+        );
+    });
+
+
+    logoutButton.addEventListener("click", function () {
+        const password = localStorage.getItem("password");
+        const authenticated = localStorage.getItem("authenticated");
+
+        if (authenticated !== null || authenticated === "true" || authenticated === true) {
+            localStorage.setItem("authenticated", false);
+            console.log(localStorage.getItem("authenticated"));
+            // wait for 1 second before redirecting
+            // setTimeout(() => {
+            //     window.location.href = "settings.html";
+            // }, 1000);
+            window.location.href = "barrier.html";
+        }
+    });
 });
